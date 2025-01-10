3df9e7f072e45fa59f5eeef791c4aa9555b04994
Maezelle Secuya <maezelle.secuya@cvsu.edu.ph>
2025-01-11 00:18:35 +0800

    inititate create

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,34 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './entities/student.entity';
 import { Repository } from 'typeorm';
 
+
 @Injectable()
-export class StudentsService {
+export class StudentService {
   constructor(
     @InjectRepository(Student)
     private readonly studentRepository: Repository<Student>,
-  ){}
-  create(createStudentDto: CreateStudentDto) {
-    return this.studentRepository.create(createStudentDto);
+  ) {}
+
+  async createStudent(studentData: CreateStudentDto): Promise<Student> {
+    const student = this.studentRepository.create(studentData);
+    return this.studentRepository.save(student);
   }
 
-  findAll() {
-    return this.studentRepository.find();
+  async findAll(): Promise<Student[]> {
+    return this.studentRepository.find(); 
   }
-
-  findOne(id: number) {
-    return this.studentRepository.findOneBy({ id });
-  }
-
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return this.studentRepository.update(id, updateStudentDto);
-  }
-
-  remove(id: number) {
-    return this.studentRepository.delete(id);
-  }
-
 }
