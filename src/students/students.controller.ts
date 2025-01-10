dc8ed79006d0845420a9f680ce50cf915abd7507
Maezelle Secuya <maezelle.secuya@cvsu.edu.ph>
2025-01-11 01:27:18 +0800

    initalize delete

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Patch, Param, Body } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { StudentService } from './students.service';
 import { UpdateStudentDto } from './dto/update-student.dto';
+
 
 @Controller('students')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentService.updateStudent(+id, updateStudentDto);  // Cast id to number
-  }
+  @Get()
+findAll() {
+  return this.studentService.findAll();
+}
+
+  
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './entities/student.entity';
 import { Repository } from 'typeorm';
-import { UpdateStudentDto } from './dto/update-student.dto';
 
 
 
@@ -12,8 +11,10 @@ export class StudentService {
     @InjectRepository(Student)
     private readonly studentRepository: Repository<Student>,
   ){}
-  async updateStudent(id: number, updateData: UpdateStudentDto): Promise<Student> {
-    await this.studentRepository.update(id, updateData);
-    return this.studentRepository.findOne({ where: { id } });
+  async findAll(): Promise<Student[]> {
+    return this.studentRepository.find();
   }
+  
+  
+  
 }
